62691a3ca76b775847f24c9b32d7f8a92148375b
NatMuHe98 <a20202943@pucp.edu.pe>
2020-05-22 15:23:48 -0500

    Actualizacion de archivos

---
+++ app/src/main/java/com/example/laboratorio2/CrearTrabajo.java
@@ -0,0 +1,46 @@
+package com.example.laboratorio2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CrearTrabajo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_trabajo);
+
+        setTitle("Crear Trabajo");
+    }
+
+
+
+    public void FinalizarRegistro(View view){
+        //Guardar en web service
+        EditText editTextNombreEmpleo = findViewById(R.id.editTextNombreEmpleo);
+        String nombreEmpleo = editTextNombreEmpleo.getText().toString();
+        EditText editTextSalarioMinimo = findViewById(R.id.editTextSalarioMinimo);
+        String salarioMinimo = editTextSalarioMinimo.getText().toString();
+        EditText editTextSalarioMaximo = findViewById(R.id.editTextSalarioMaximo);
+        String salarioMaximo = editTextSalarioMaximo.getText().toString();
+        EditText editTextAbreviación = findViewById(R.id.editTextAbreviacion);
+        String abreviacion = editTextAbreviación.getText().toString();
+
+        if(nombreEmpleo.isEmpty()){
+            editTextNombreEmpleo.setError("Por favor ingrese el nombre");
+        }else if(salarioMinimo.isEmpty()){
+            editTextSalarioMinimo.setError("Por favor ingrese el salario mínimo");
+        }else if(salarioMaximo.isEmpty()){
+            editTextSalarioMaximo.setError("Por favor ingrese el salario maximo");
+        }else if(abreviacion.isEmpty()){
+            editTextAbreviación.setError("Por favor ingrese una abreviación al empleo");
+        }
+        else{
+            finish();
+        }
+
+    }
+}
