ed4f54a8652941b7e54b94c9aa5c965dd7eb775c
Brayan Dadick <dadick.monzon@pucp.edu.pe>
2020-05-23 10:27:33 -0500

    CrearEmpleado_upd1

--- app/src/main/java/com/example/laboratorio2/CrearEmpleado.java
+++ app/src/main/java/com/example/laboratorio2/CrearEmpleado.java
@@ -3,6 +3,8 @@ package com.example.laboratorio2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CrearEmpleado extends AppCompatActivity {
 
@@ -12,6 +14,26 @@ public class CrearEmpleado extends AppCompatActivity {
         setContentView(R.layout.activity_crear_empleado);
         setTitle("Crear Empleado");
 
+    
+    }
+
+
+    public void finalizarCrearEmpleado (){
+
+    EditText editTextCEnombre = findViewById(R.id.editTextCEnombre);
+    String CEnombre = editTextCEnombre.getText().toString();
+    EditText editTextCEapellido = findViewById(R.id.editTextCEapellido);
+    String CEapellido = editTextCEapellido.getText().toString();
+    EditText editTextCEemail = findViewById(R.id.editTextCEemail);
+    String CEemail = editTextCEemail.getText().toString();
+    EditText editTextCEcelular = findViewById(R.id.editTextCEcelular);
+    String CEcelular = editTextCEcelular.getText().toString();
+    EditText editTextCEsalario =  findViewById(R.id.editTextCEsalario);
+    String CEsalario = editTextCEsalario.getText().toString();
+    EditText editTextCEcomision = findViewById(R.id.editTextCEcomision);
+    String CEcomision = editTextCEcomision.getText().toString();
+    
+    Button buttonGrabarEmpleado = findViewById(R.id.buttonCEgrabar);
 
     }
 }
