c0c7af0b7d250f94659104fb4025d918292b28ce
Brayan Dadick <dadick.monzon@pucp.edu.pe>
2020-05-23 14:20:34 -0500

    DiseñoCE1

--- app/src/main/java/com/example/laboratorio2/CrearEmpleado.java
+++ app/src/main/java/com/example/laboratorio2/CrearEmpleado.java
@@ -2,7 +2,9 @@ package com.example.laboratorio2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,7 +20,20 @@ public class CrearEmpleado extends AppCompatActivity {
     }
 
 
-    public void finalizarCrearEmpleado (){
+    public void finalizarCrearEmpleado (View view){
+        //Se activa con el boton siguiente
+        //Button buttonGrabarEmpleado = findViewById(R.id.buttonCEgrabar);
+
+        //Se debe iniciar de la lista empleados un intent
+        /*Button b = finViewById(R.id.buttonagregarpersonadelabarra);
+        b.SetOnClickListener(new View.OnclickListener(){
+        public void onClick(View v)
+        Intent intent = new Intent(activity_empleados_list.this, activity_crear_empleado.class);
+        int requestCode =1;
+        startActivityForResult(intent,requestCode);
+        }
+         */
+
 
     EditText editTextCEnombre = findViewById(R.id.editTextCEnombre);
     String CEnombre = editTextCEnombre.getText().toString();
@@ -32,8 +47,12 @@ public class CrearEmpleado extends AppCompatActivity {
     String CEsalario = editTextCEsalario.getText().toString();
     EditText editTextCEcomision = findViewById(R.id.editTextCEcomision);
     String CEcomision = editTextCEcomision.getText().toString();
-    
-    Button buttonGrabarEmpleado = findViewById(R.id.buttonCEgrabar);
+
+
+    Intent intentGuardar = new Intent();
+    //Subir en formato JSON
+    setResult(RESULT_OK,intentGuardar);
+    finish();
 
     }
 }
