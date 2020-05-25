c9b52e416197b3b0b1d20c40bf8b23a76421ae3f
a20202918 <64922858+a20202918@users.noreply.github.com>
2020-05-24 21:23:25 -0500

    Update Departamento.java

--- app/src/main/java/com/example/laboratorio2/entidades/Departamento.java
+++ app/src/main/java/com/example/laboratorio2/entidades/Departamento.java
@@ -1,61 +1,31 @@
 package com.example.laboratorio2.entidades;
 
 public class Departamento {
-    private int departamentId;
-    private String departamentName;
-    private String managerId;
-    private int locationId;
-    private String departamentShortName;
+    private int departmentId;
+    private String departmentName;
+    private String departmentShortName;
 
-    public Departamento() {
-
+    public int getDepartmentId() {
+        return departmentId;
     }
 
-    public Departamento(int departamentId, String departamentName, String managerId, int locationId, String departamentShortName) {
-        this.departamentId = departamentId;
-        this.departamentName = departamentName;
-        this.managerId = managerId;
-        this.locationId = locationId;
-        this.departamentShortName = departamentShortName;
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
     }
 
-    public int getDepartamentId() {
-        return departamentId;
+    public String getDepartmentName() {
+        return departmentName;
     }
 
-    public void setDepartamentId(int departamentId) {
-        this.departamentId = departamentId;
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
     }
 
-    public String getDepartamentName() {
-        return departamentName;
+    public String getDepartmentShortName() {
+        return departmentShortName;
     }
 
-    public void setDepartamentName(String departamentName) {
-        this.departamentName = departamentName;
-    }
-
-    public String getManagerId() {
-        return managerId;
-    }
-
-    public void setManagerId(String managerId) {
-        this.managerId = managerId;
-    }
-
-    public int getLocationId() {
-        return locationId;
-    }
-
-    public void setLocationId(int locationId) {
-        this.locationId = locationId;
-    }
-
-    public String getDepartamentShortName() {
-        return departamentShortName;
-    }
-
-    public void setDepartamentShortName(String departamentShortName) {
-        this.departamentShortName = departamentShortName;
+    public void setDepartmentShortName(String departmentShortName) {
+        this.departmentShortName = departmentShortName;
     }
 }
